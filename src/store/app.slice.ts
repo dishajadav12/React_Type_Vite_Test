6b5fbace1900db1b9ba9ef891de4b974c43555ec
Disha <dishajadav12402@gmail.com>
2024-03-12 14:17:18 +0530

    appSlice added

---
+++ src/store/app.slice.ts
@@ -0,0 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const appSlice = createSlice({ name: "app", initialState: [], reducers: {} });
+
+export default appSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import appSlice from "./app.slice";
 
-export const store = configureStore({ reducer: {} });
+export const store = configureStore({
+  reducer: {
+    appSlice: appSlice,
+  },
+});
 
 export type Rootstate = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
